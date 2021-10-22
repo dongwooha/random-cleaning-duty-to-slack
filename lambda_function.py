6b9ecede479b795cec196454ba9d8ff9e4a43874
jay <jaeykweon@naver.com>
2021-10-22 17:31:40 +0900

    [refactor] randrange -> choice

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ def lambda_handler(event, context):
     from os import environ
     from slack_sdk import WebClient
     from slack_sdk.errors import SlackApiError
-    from random import randrange
+    from random import choice
 
     client = WebClient(token=environ['SLACK_TOKEN'])
     duty_member_list = environ['MEMBERS'].split(',')
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     try:
         response = client.chat_postMessage(
             channel=environ['SLACK_CHANNEL'],
-            text="오늘의 청소당번은 "+ str(duty_member_list[ randrange(0, len(duty_member_list)-1) ]) + "님 입니다."
+            text=f"오늘의 청소당번은 {choice(duty_member_list)}님 입니다."
         )
         print(response)
     except SlackApiError as e:
