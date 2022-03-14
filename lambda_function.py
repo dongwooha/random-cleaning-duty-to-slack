84955ffa20781a5568c564ba8592b2d14df4085c
dongwooha <dongwoo.ha@gmail.com>
2022-03-15 00:29:37 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,13 @@
+import random
+
 def lambda_handler(event, context):
 
     from os import environ
     from slack_sdk import WebClient
     from slack_sdk.errors import SlackApiError
     from random import choice
+    from random import sample
+
 
     client = WebClient(token=environ['SLACK_TOKEN'])
     duty_member_list = environ['MEMBERS'].split(',')
@@ -11,7 +15,7 @@ def lambda_handler(event, context):
     try:
         response = client.chat_postMessage(
             channel=environ['SLACK_CHANNEL'],
-            text=f"오늘의 청소당번은 {choice(duty_member_list)}님 입니다."
+            text=f"이번주 밥 멤버는 {sample(duty_member_list,3)} 입니다."
         )
         print(response)
     except SlackApiError as e:
